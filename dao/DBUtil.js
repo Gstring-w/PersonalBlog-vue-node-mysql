0bc597c7631d4480d59e3d3bfb3309d57e76f9d8
Gstring-w <gstring_w@163.com>
2019-03-08 15:24:59 +0800

    mysql

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -5,7 +5,7 @@ function createConnection() {
         host: "localhost",
         port: "3306",
         user: "root",
-        password: "123456",
+        password: "new password",
         database: "my_blog"
     });
     return connection;
